42e04603699707dcf54433a9441a00ea4c7282d9
BeanBeam <s110819015@stu.ntue.edu.tw>
2022-03-06 15:11:38 +0800

    add Drawer, refactor code of Stack and Tab

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,36 +1,38 @@
 import React from 'react';
-import { StyleSheet, Image, Pressable } from 'react-native';
+import { StyleSheet, Image, Pressable, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
 
 import HomeScreen from '../screens/HomeScreen';
 import WishlistScreen from '../screens/WishlistScreen';
 import MyBooksScreen from '../screens/MyBooksScreen';
 import BookInfoScreen from '../screens/BookInfoScreen';
 
-const HomeStack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
 const Navigation = () => {
   return (
     <NavigationContainer>
-      <TabNavigator />
+      <DrawerNavigator />
     </NavigationContainer>
   );
 }
 
-const HomeStackScreen = () => {
+const HomeStackNavigator = () => {
     return (
-      <HomeStack.Navigator
+      <Stack.Navigator
         screenOptions={{
             headerShadowVisible: false,
         }}
       >
-        <HomeStack.Screen
+        <Stack.Screen
             name="HomeScreen"
             component={HomeScreen}
-            options={{
+            options={({ navigation }) => ({
                 headerTitle: '',
                 headerRight: () => (
                     <Pressable onPress={() => alert('Search')}>
@@ -38,13 +40,13 @@ const HomeStackScreen = () => {
                     </Pressable>
                 ),
                 headerLeft: () => (
-                    <Pressable onPress={() => alert('Drawer')}>
+                    <Pressable onPress={() => navigation.toggleDrawer()}>
                         <Image source={require('../../assets/icons/icon_menu.png')} style={styles.navIcon}/>
                     </Pressable>
                 ),
-            }}
+            })}
         />
-        <HomeStack.Screen
+        <Stack.Screen
             name="BookInfo"
             component={BookInfoScreen}
             options={({ navigation }) => ({
@@ -61,7 +63,53 @@ const HomeStackScreen = () => {
                 ),
             })}
         />
-      </HomeStack.Navigator>
+      </Stack.Navigator>
+    );
+}
+
+const WishlistStackNavigator = () => {
+    return (
+        <Stack.Navigator
+          screenOptions={{
+              headerShadowVisible: false,
+          }}
+        >
+          <Stack.Screen
+              name="WishlistScreen"
+              component={WishlistScreen}
+              options={({ navigation }) => ({
+                headerTitle: '',
+                headerRight: () => (
+                    <Pressable onPress={() => alert('Search')}>
+                        <Image source={require('../../assets/icons/icon_search.png')} style={styles.navIcon}/>
+                    </Pressable>
+                ),
+              })}
+          />
+        </Stack.Navigator>
+    );
+}
+
+const MyBooksStackNavigator = () => {
+    return (
+        <Stack.Navigator
+          screenOptions={{
+              headerShadowVisible: false,
+          }}
+        >
+          <Stack.Screen
+              name="MyBooksScreen"
+              component={MyBooksScreen}
+              options={({ navigation }) => ({
+                headerTitle: '',
+                headerRight: () => (
+                    <Pressable onPress={() => alert('Search')}>
+                        <Image source={require('../../assets/icons/icon_search.png')} style={styles.navIcon}/>
+                    </Pressable>
+                ),
+              })}
+          />
+        </Stack.Navigator>
     );
 }
 
@@ -100,10 +148,84 @@ const TabNavigator = () => {
                 },
             })}
         >
-            <Tab.Screen name="Home" component={HomeStackScreen} />
-            <Tab.Screen name="Wishlist" component={WishlistScreen} />
-            <Tab.Screen name="MyBooks" component={MyBooksScreen} />
+            <Tab.Screen name="Home" component={HomeStackNavigator} />
+            <Tab.Screen name="Wishlist" component={WishlistStackNavigator} />
+            <Tab.Screen name="MyBooks" component={MyBooksStackNavigator} />
       </Tab.Navigator>
+    );
+}
+
+const DrawerNavigator = () => {
+    return (
+        <Drawer.Navigator
+            initialRouteName="HomeDrawer"
+            screenOptions={{
+                headerShown: false,
+                drawerActiveTintColor: '#6200EE',
+                drawerLabelStyle: {
+                    fontSize: 14,
+                    lineHeight: 16,
+                    letterSpacing: 0.012
+                },
+            }}
+            drawerContent={(props) => <CustomDrawerContent {...props} />}
+        >
+            <Drawer.Screen name="HomeDrawer" component={TabNavigator} options={{
+                drawerLabel: 'Home',
+                drawerIcon: ({focused}) => (
+                    focused
+                    ? <Image source={require('../../assets/icons/icon_home_actived.png')} style={styles.navIcon} />
+                    : <Image source={require('../../assets/icons/icon_home.png')} style={styles.navIcon} />
+                )
+            }}/>
+            <Drawer.Screen name="WishlistDrawer" component={WishlistScreen} options={{
+                drawerLabel: 'Wishlist',
+                drawerIcon: ({focused}) => (
+                    focused
+                    ? <Image source={require('../../assets/icons/icon_nav_bookmark_actived.png')} style={styles.navIcon} />
+                    : <Image source={require('../../assets/icons/icon_nav_bookmark.png')} style={styles.navIcon} />
+                )
+            }}/>
+            <Drawer.Screen name="MyBooksDrawer" component={MyBooksScreen} options={{
+                drawerLabel: 'My books',
+                drawerIcon: ({focused}) => (
+                    focused
+                    ? <Image source={require('../../assets/icons/icon_mybook_actived.png')} style={styles.navIcon} />
+                    : <Image source={require('../../assets/icons/icon_mybook.png')} style={styles.navIcon} />
+                )
+            }}/>
+            <Drawer.Screen name="AccountDrawer" component={HomeStackNavigator} options={{
+                drawerLabel: 'Account',
+                drawerIcon: ({focused}) => (
+                    focused
+                    ? <Image source={require('../../assets/icons/icon_account_actived.png')} style={styles.navIcon} />
+                    : <Image source={require('../../assets/icons/icon_account.png')} style={styles.navIcon} />
+                )
+            }}/>
+            <Drawer.Screen name="SettingDrawer" component={HomeStackNavigator} options={{
+                drawerLabel: 'Setting',
+                drawerIcon: ({focused}) => (
+                    focused
+                    ? <Image source={require('../../assets/icons/icon_settings_actived.png')} style={styles.navIcon} />
+                    : <Image source={require('../../assets/icons/icon_settings.png')} style={styles.navIcon} />
+                )
+            }}/>
+        </Drawer.Navigator>
+    );
+}
+
+const CustomDrawerContent = (props) => {
+    return (
+        <DrawerContentScrollView {...props}>
+        <View style={styles.drawerHeader}>
+            <View style={styles.avatarBox}>
+                <Image source={require('../../assets/book_images/img_book_tbos.png')} style={styles.avatar} resizeMode={'cover'} />
+            </View>
+            <Text style={styles.username}>Username</Text>
+        </View>
+        <DrawerItemList {...props} />
+        {/* <DrawerItem label="Help" onPress={() => alert('Link to help')} /> */}
+        </DrawerContentScrollView>
     );
 }
 
@@ -113,5 +235,31 @@ const styles = StyleSheet.create({
     navIcon: {
         width: 24,
         height: 24,
+    },
+    drawerHeader: {
+        backgroundColor: '#fff',
+        borderBottomWidth: 1,
+        borderColor: '#EDEDEF',
+        // paddingTop: 40,
+        paddingHorizontal: 16,
+        paddingBottom: 16,
+    },
+    avatarBox: {
+        width: 48,
+        height: 48,
+        borderRadius: 24
+    },
+    avatar: {
+        width: 48,
+        height: 48,
+        borderRadius: 24
+    },
+    username: {
+        color: '#131313',
+        fontSize: 24,
+        fontWeight: '500',
+        lineHeight: 28,
+        letterSpacing: 0.3,
+        marginTop: 16,
     }
 });
